2e668e550e8c821cf2d18214133d416e3f304f2f
Spaceocean <spaceocean@outlook.com>
2024-11-13 19:18:44 +0500

    added comments

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import random
 
 
-def generate_number():
+def generate_number() -> str:
+    """ Возвращает четырехзначное число из уникальных чисел """
     numbers = [str(i) for i in range(1, 10)]
     random.shuffle(numbers)
     list_numbers = []
@@ -12,7 +13,8 @@ def generate_number():
             list_numbers.append(number)
 
 
-def checking_user_number_for_letters(number: str):
+def checking_user_number_for_letters(number: str) -> bool:
+    """ Проверяет входящее значение на цифровые символы """
     for n in number:
         if not n.isdigit():
             print('Некорректный запрос: вы ввели не число')
@@ -20,28 +22,31 @@ def checking_user_number_for_letters(number: str):
     return True
 
 
-def checking_user_number_for_length(number: str):
+def checking_user_number_for_length(number: str) -> bool:
+    """ Проверяет входящее значение на длину """
     if len(number) != 4:
         print('Некорректный запрос: число должно быть четырехзначным')
         return False
     return True
 
 
-def checking_user_number_for_uniqueness(number: str):
+def checking_user_number_for_uniqueness(number: str) -> bool:
+    """ Проверяет входящее значение на уникальность символов"""
     if len(list(number)) != len(set(number)):
         print('Некорректный запрос: вы ввели неуникальные цифры')
         return False
     return True
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     print('Игра "Быки и коровы"')
     print('Нужно отгадать четырехзначное число из уникальных цифр')
+    # Получаем загаданное число
     secret_number = generate_number()
-    print(secret_number)
     while True:
         bulls, cows = 0, 0
         user_number = input('Введите число: ')
+        # Проверяем число на корректность
         checks = [checking_user_number_for_letters,
                   checking_user_number_for_length,
                   checking_user_number_for_uniqueness]
@@ -50,8 +55,9 @@ if __name__ == "__main__":
                 break
         else:
             if user_number == secret_number:
-                print('Число угадано')
+                print('Число угадано!')
                 break
+            # Находим количество быков и коров в переданном числе
             for num in user_number:
                 if num in secret_number and user_number.index(num) == secret_number.index(num):
                     bulls += 1
